8b5311ba519eae878257b0fc8324d3a2043ddcd0
bahati <bahatifidelis2004@gmail.com>
2025-07-09 10:19:40 +0300

    fixing react-mde in editor.jsx

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,10 +1,9 @@
+import React from "react";
+import ReactMde from "react-mde"; // Changed from MDEditor
+import "react-mde/lib/styles/css/react-mde-all.css";
+import Showdown from "showdown";
 
-import React from "react"
-import ReactMde from "react-mde"  
-import "react-mde/lib/styles/css/react-mde-all.css" 
-import Showdown from "showdown"
-
-export default function Editor({ tempNoteText, setTempNoteText }) {
+export default function Editor({ currentNote, updateNote }) {
     const [selectedTab, setSelectedTab] = React.useState("write")
 
     const converter = new Showdown.Converter({
@@ -12,13 +11,13 @@ export default function Editor({ tempNoteText, setTempNoteText }) {
         simplifiedAutoLink: true,
         strikethrough: true,
         tasklists: true,
-    })
+    })  
 
     return (
         <section className="pane editor">
             <ReactMde
-                value={tempNoteText}
-                onChange={setTempNoteText}
+                value={currentNote.body}
+                onChange={updateNote}
                 selectedTab={selectedTab}
                 onTabChange={setSelectedTab}
                 generateMarkdownPreview={(markdown) =>
